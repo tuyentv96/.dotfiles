4b356a2548f92dfe87527b460de9c06e4c1fa0e7
Tuyen Tran <tuyen@Tuyens-MacBook-Pro.local>
2021-12-29 18:41:53 +0700

    Enhance performance

--- nvim/.config/nvim/lua/plugins/config/nvimtree.lua
+++ nvim/.config/nvim/lua/plugins/config/nvimtree.lua
@@ -44,14 +44,14 @@ local list = {
   { key = "g?",                           cb = tree_cb("toggle_help") },
 }
 
-vim.g.nvim_tree_quit_on_open = 0
+vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_indent_markers = 0
-vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_git_hl = 0
 vim.g.nvim_tree_highlight_opened_files = 0
 vim.g.nvim_tree_root_folder_modifier = ':~'
 vim.g.nvim_tree_add_trailing = 0
 vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_disable_window_picker = 0
+vim.g.nvim_tree_disable_window_picker = 1
 vim.g.nvim_tree_icon_padding = ' '
 vim.g.nvim_tree_symlink_arrow = ' >> '
 vim.g.nvim_tree_respect_buf_cwd = 0
