d27a9d341856dd7c17ac19b2bd3b2af31299bd91
Tuyen Tran <tuyen@Tuyens-MacBook-Pro.local>
2022-01-17 21:34:51 +0700

    Fix toggleterm

--- nvim/.config/nvim/lua/plugins/config/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/config/toggleterm.lua
@@ -9,7 +9,7 @@ toggleterm.setup{
   open_mapping = [[<c-t>]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
-  shade_terminals = false,
+  shade_terminals = true,
   shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
