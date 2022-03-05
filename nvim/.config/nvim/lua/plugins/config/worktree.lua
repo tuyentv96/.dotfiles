14238e5b794d4f0a05890630d5abc10ce84dd9e8
Tuyen Tran <tuyen@Tuyens-MacBook-Pro.local>
2022-03-05 14:07:31 +0700

    Add git worktree

--- nvim/.config/nvim/lua/plugins/config/worktree.lua
+++ nvim/.config/nvim/lua/plugins/config/worktree.lua
@@ -1,1 +1,15 @@
--- require("git-worktree").setup()
+local Worktree = require("git-worktree")
+
+Worktree.setup({
+    change_directory_command = "cd",
+    update_on_change = true,
+    update_on_change_command = "e .",
+    clearjumps_on_change = true
+})
+
+Worktree.on_tree_change(function(op, metadata)
+  if op == Worktree.Operations.Switch then
+    require'nvim-tree.view'.close()
+    require'nvim-tree'.open(metadata.path)
+  end
+end)
