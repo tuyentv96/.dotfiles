ffa04c9eb7b83f991ef578ce1bcb43f6762e3c79
Tuyen Tran <tuyen@Tuyens-MacBook-Pro.local>
2022-03-05 14:25:29 +0700

    Fix git

--- nvim/.config/nvim/lua/plugins/config/git.lua
+++ nvim/.config/nvim/lua/plugins/config/git.lua
@@ -8,7 +8,7 @@ vim.g.fugitive_dynamic_colors = 0
 
 nnoremap("<leader>ga", ":Git fetch --all<CR>")
 nnoremap("<leader>gp", ":Git -c push.default=current push<CR>")
-nnoremap("<leader>gr", ":Git rebase origin/master<CR>")
+nnoremap("<leader>gr", ":Git rebase master<CR>")
 nnoremap("<leader>gf", ":Git pull --rebase<CR>")
 nnoremap("<leader>gv", ":GV <CR>")
 nnoremap("<leader>gc", ":G commit<CR>")
