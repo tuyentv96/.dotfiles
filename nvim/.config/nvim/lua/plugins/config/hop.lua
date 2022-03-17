fdbc6996513be2ab39e8e3aaaa091d7e08c995a6
Tuyen Tran <tuyen@Tuyens-MacBook-Pro.local>
2022-03-17 08:41:28 +0700

    Fix hop

--- nvim/.config/nvim/lua/plugins/config/hop.lua
+++ nvim/.config/nvim/lua/plugins/config/hop.lua
@@ -4,26 +4,29 @@ if not present then
 end
 
 require("core.utils")
-hop.setup()
+hop.setup({
+  case_insensitive = true,
+  char2_fallback_key = '<ESC>',
+})
 
-nnoremap("s","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
+-- nnoremap("s","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
 -- vnoremap("s","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
 
-nnoremap("S","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
+-- nnoremap("S","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
 -- vnoremap("S","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=true })<cr>")
 
-nnoremap("f","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
-vnoremap("f","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
-onoremap("f","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=true })<cr>")
+nnoremap("f","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
+vnoremap("f","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
+onoremap("f","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=true })<cr>")
 
-nnoremap("F","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
-vnoremap("F","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
-onoremap("F","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
+nnoremap("F","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
+vnoremap("F","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
+onoremap("F","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
 
-nnoremap("t","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
-vnoremap("t","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
-onoremap("t","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
+nnoremap("t","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
+vnoremap("t","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
+onoremap("t","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.AFTER_CURSOR, inclusive_jump=false })<cr>")
 
-nnoremap("T","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
-vnoremap("T","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=true})<cr>")
-onoremap("T","<cmd>lua require'hop'.hint_char1({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false})<cr>")
+nnoremap("T","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false })<cr>")
+vnoremap("T","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=true})<cr>")
+onoremap("T","<cmd>lua require'hop'.hint_char2({ direction=require'hop.hint'.HintDirection.BEFORE_CURSOR, inclusive_jump=false})<cr>")
