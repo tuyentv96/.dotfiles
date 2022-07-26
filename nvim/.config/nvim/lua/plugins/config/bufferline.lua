3a75408386b59cbd0ff919de1ddcf2e5736ea503
Tuyen Tran <vat1906@gmail.com>
2022-07-26 22:04:54 +0700

    Fix bufferline

--- nvim/.config/nvim/lua/plugins/config/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/config/bufferline.lua
@@ -38,7 +38,7 @@ bufferline.setup {
         -- buffer will be hidden.
 
         -- filter out filetypes you don't want to see
-        local exclude_ft    = { "git", "NvimTree", "dap-repl" }
+        local exclude_ft    = { "git", "NvimTree", "dap-repl", "fugitive" }
         local cur_ft        = vim.bo[bufnr].filetype
         local should_filter = vim.tbl_contains(exclude_ft, cur_ft)
 
