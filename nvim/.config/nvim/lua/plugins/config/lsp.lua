47cb16ad43f4baad5aa0aff0e7e8cfc5b18fae08
Tuyen Tran <vat1906@gmail.com>
2022-07-30 16:41:27 +0700

    Disable lua lsp

--- nvim/.config/nvim/lua/plugins/config/lsp.lua
+++ nvim/.config/nvim/lua/plugins/config/lsp.lua
@@ -59,27 +59,27 @@ lsp_config.gopls.setup(utils.lsp_config({
 lsp_config.dartls.setup{}
 
 -- lua
-lsp_config.sumneko_lua.setup(utils.lsp_config({
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-  settings = {
-      Lua = {
-          runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT',
-              -- Setup your lua path
-              path = vim.split(package.path, ';')
-          },
-          diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = {'vim'}
-          },
-          workspace = {
-            maxPreload = 5000, -- Add this if missing or increase it
-            preloadFileSize = 5000, -- Add this if missing or increase it
-              -- Make the server aware of Neovim runtime files
-              library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
-          }
-      }
-  }
-}))
+-- lsp_config.sumneko_lua.setup(utils.lsp_config({
+--   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+--   settings = {
+--       Lua = {
+--           runtime = {
+--               -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+--               version = 'LuaJIT',
+--               -- Setup your lua path
+--               path = vim.split(package.path, ';')
+--           },
+--           diagnostics = {
+--               -- Get the language server to recognize the `vim` global
+--               globals = {'vim'}
+--           },
+--           workspace = {
+--             maxPreload = 5000, -- Add this if missing or increase it
+--             preloadFileSize = 5000, -- Add this if missing or increase it
+--               -- Make the server aware of Neovim runtime files
+--               library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+--           }
+--       }
+--   }
+-- }))
 
