d91522512f21af27f4c85089609e0bbb673fda4a
Tuyen Tran <vat1906@gmail.com>
2022-08-28 21:19:36 +0700

    Enfore regular tab

--- nvim/.config/nvim/lua/plugins/config/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/config/bufferline.lua
@@ -24,7 +24,7 @@ bufferline.setup {
         max_prefix_length       = 0,
         tab_size                = 20,
         show_tab_indicators     = false,
-        enforce_regular_tabs    = false,
+        enforce_regular_tabs    = true,
         -- view                    = "multiwindow",
         show_buffer_close_icons = false,
         separator_style         = "thin",
