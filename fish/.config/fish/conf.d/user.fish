a9910b4149b19e52e02218fca3cb547e530b7751
Tuyen Tran <vat1906@gmail.com>
2022-09-17 02:54:29 +0700

    Fix fish bind

--- fish/.config/fish/conf.d/user.fish
+++ fish/.config/fish/conf.d/user.fish
@@ -1,9 +1,6 @@
+function fish_user_key_bindings
 # clear terminal 
-function fish_user_key_bindings
     bind \cn 'clear; commandline -f repaint'
-end
-
 # exit
-function fish_user_key_bindings
     bind \cs 'exit'
 end
