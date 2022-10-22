c85f2ed55fd84f656a8a3648e293902cefefa68c
Tuyen Tran <tuyen@pop-os.localdomain>
2022-10-22 10:11:33 +0700

    Enable toggleterm

--- nvim/.config/nvim/lua/plugins/config/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/config/toggleterm.lua
@@ -1,28 +1,29 @@
--- local present, toggleterm = pcall(require, "toggleterm")
--- if not present then
---    return
--- end
--- require("core.utils")
+local present, toggleterm = pcall(require, "toggleterm")
+if not present then
+   return
+end
+require("core.utils")
 
--- toggleterm.setup{
---   -- size can be a number or function which is passed the current terminal
---   size = 20,
---   -- open_mapping = [[<c-t>]],
---   hide_numbers = true, -- hide the number column in toggleterm buffers
---   shade_filetypes = {},
---   shade_terminals = true,
---   shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
---   start_in_insert = false,
---   insert_mappings = true, -- whether or not the open mapping applies in insert mode
---   persist_size = true,
---   direction = "horizontal", -- 'vertical' | 'horizontal' | 'window' | 'float',
---   close_on_exit = true, -- close the terminal window when the process exits
---   shell = "/usr/local/bin/fish",
--- }
+toggleterm.setup{
+  -- size can be a number or function which is passed the current terminal
+  size = 20,
+  open_mapping = [[<c-t>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  start_in_insert = true,
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  persist_size = true,
+  direction = "float", -- 'vertical' | 'horizontal' | 'window' | 'float',
+  close_on_exit = true, -- close the terminal window when the process exits
+  shell = "/usr/local/bin/fish",
+}
 
+tnoremap("<ESC>", "<C-\\><C-n>")
 -- -- vim.api.nvim_set_keymap("t", "<ESC>", '<C-\\><C-n>', {noremap = true, silent = true})
--- nnoremap("<C-y>", "<cmd>:ToggleTerm direction=tab<CR>")
 -- tnoremap("<C-y>", "<cmd>:ToggleTerm direction=tab<CR>")
+-- nnoremap("<C-y>", "<cmd>:ToggleTerm direction=tab<CR>")
 -- nnoremap("<C-t>", "<cmd>:ToggleTerm direction=horizontal<CR>")
 -- tnoremap("<C-t>", "<cmd>:ToggleTerm direction=horizontal<CR>")
 -- tnoremap("<ESC>", "<C-\\><C-n>")
